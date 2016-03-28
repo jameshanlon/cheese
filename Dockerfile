9623706e3f2bd8bffc61a31940bec0a153d9ed86
James Hanlon <jameswhanlon@gmail.com>
2016-03-28 18:27:21 +0100

    Various updates and Docker container.

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM nginx
+MAINTAINER James Hanlon
+
+# Nginx config.
+RUN mkdir /etc/nginx/sites-available && \
+    mkdir /etc/nginx/sites-enabled
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY nginx-cheese.conf /etc/nginx/sites-available/
+RUN rm /etc/nginx/conf.d/default.conf && \
+    ln -s /etc/nginx/sites-available/nginx-cheese.conf \
+          /etc/nginx/sites-enabled/nginx-cheese.conf
+
+EXPOSE 9080
+CMD service nginx start
---
+++ deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+usage() {
+  echo usage: `basename $0` "<option>"
+  exit 1
+}
+
+rebuild() {
+  echo "Rebuilding containers..."
+  
+  # Remove the existing containers.
+  docker rm website
+
+  # (Re)build the website container.
+  docker build -t website .
+
+  # Create the website container and link with the db.
+  docker create --name website \
+    -p 9080:9080 \
+    -v `pwd`:/opt/www \
+    website
+}
+
+up() {
+  echo "Starting containers..."
+  docker start website
+  sleep 5
+  echo "\nWebsite container logs:"
+  docker logs website
+}
+
+down() {
+  echo "Stopping containers..."
+  docker stop website
+}
+
+restart() {
+  echo "Restarting website processes..."
+  # Restart processes with supervisorctl.
+  docker exec -it website \
+    service nginx restart
+}
+
+refresh() {
+  down
+  rebuild
+  up
+}
+
+# Perform an action.
+case "$1" in
+  "down" )
+    down;;
+  "up" )
+    up;;
+  "restart" )
+    restart;;
+  "rebuild" )
+    rebuild;;
+  "refresh" )
+    refresh;;
+  *)
+    echo ERROR: $* 1>&2
+    usage;;
+esac
