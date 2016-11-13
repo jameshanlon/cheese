b401969db653d3deb061fb62e2f6cfaff902e6ff
James Hanlon <jameswhanlon@gmail.com>
2016-11-13 14:09:09 -0500

    Fix backup script.

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Performing backup..."
 DATE=$(date +"%m-%d-%y-%H-%M")
-FILENAME="backup-${CHEESE_MYSQL_DATABASE}_${DATE}.sql"
+FILENAME="${CHEESE_BACKUP_DIR}/backup-${CHEESE_MYSQL_DATABASE}_${DATE}.sql"
 # Do a mysqldump.
 docker exec -it ${CHEESE_CONTAINER_NAME} \
-	mysqldump $MYSQL_DATABASE > ./$FILENAME
+	mysqldump $CHEESE_MYSQL_DATABASE > $FILENAME
 echo "Done: created '$FILENAME'"
