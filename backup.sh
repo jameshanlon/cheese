a95517de4f8832c3c4c27d1cf8b371f9332add1e
James Hanlon <jameswhanlon@gmail.com>
2016-11-13 09:06:42 -0500

    Fix backup script variables.

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@ echo "Performing backup..."
 DATE=$(date +"%m-%d-%y-%H-%M")
 FILENAME="backup-${CHEESE_MYSQL_DATABASE}_${DATE}.sql"
 # Do a mysqldump.
-docker exec -it ${CHEESE_MYSQL_DATABASE} \
+docker exec -it ${CHEESE_CONTAINER_NAME} \
 	mysqldump $MYSQL_DATABASE > ./$FILENAME
 echo "Done: created '$FILENAME'"
