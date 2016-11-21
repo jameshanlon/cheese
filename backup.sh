aa5dc36f8059cabbfffb5a222694cf376b1da8ea
James Hanlon <jameswhanlon@gmail.com>
2016-11-21 13:40:32 -0500

    Remove old backups.

--- backup.sh
+++ backup.sh
@@ -5,4 +5,7 @@ FILENAME="${CHEESE_BACKUP_DIR}/backup-${CHEESE_MYSQL_DATABASE}_${DATE}.sql"
 # Do a mysqldump.
 docker exec -it ${CHEESE_CONTAINER_NAME} \
 	mysqldump $CHEESE_MYSQL_DATABASE > $FILENAME
+# Remove backups older than 30 days.
 echo "Done: created '$FILENAME'"
+echo "Removing old backups:"
+find ${CHEESE_BACKUP_DIR} -type f -mtime +30 -print -exec rm {} +
