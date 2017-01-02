e8599121ebe6ea508c183b89181a38a0e95c6646
James Hanlon <jameswhanlon@gmail.com>
2017-01-02 13:12:18 -0500

    Fix mysql backup script.

--- backup.sh
+++ backup.sh
@@ -9,9 +9,9 @@ echo "Performing backup..."
 DATE=$(date +"%m-%d-%y-%H-%M")
 FILENAME="${CHEESE_BACKUP_DIR}/backup-${CHEESE_MYSQL_DATABASE}_${DATE}.sql"
 # Do a mysqldump.
-docker exec -it ${CHEESE_CONTAINER_NAME} \
+docker exec ${CHEESE_CONTAINER_NAME} \
 	mysqldump $CHEESE_MYSQL_DATABASE > $FILENAME
 # Remove backups older than 90 days.
-echo "Done: created '$FILENAME'"
+echo "Created '$FILENAME'"
 echo "Removing old backups:"
 find ${CHEESE_BACKUP_DIR} -type f -mtime +90 -print -exec rm {} +
