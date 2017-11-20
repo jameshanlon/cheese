cfa1781d689df3475d1607c7a830a5cd93f2265b
Jamie Hanlon <jamie@graphcore.ai>
2017-11-20 22:27:22 +0000

    Add JQuery Chosen to provide survey select box search.

--- cheese/static/js/main.js
+++ cheese/static/js/main.js
@@ -1,18 +1,4 @@
 jQuery(function($) {
-  $(".pop").on("click", function() {
-    // Get the image src and convert the thumbnail URL.
-    var src = $(this).find('img').attr('src');
-    src = src.replace('/static/images/thumbs', '');
-    var regex = /(.*)_\d+x\d+_\d+\.(.*)/;
-    src = src.replace(regex, '$1.$2')
-    $('#imagepreview').attr('src', src);
-    // Set the alt text.
-    var alt_text = $(this).find('img').attr('alt');
-    if (alt_text == null) {
-      $('#imagepreview-caption').text('');
-    } else {
-      $('#imagepreview-caption').text(alt_text);
-    }
-    $('#imagemodal').modal('show');
-  });
+  // Add a search function to survey select box.
+  $("#survey_form #survey").chosen();
 });
