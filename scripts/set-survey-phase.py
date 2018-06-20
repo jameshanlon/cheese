1e24efac655611f31e58186ff51fd380d50238f6
James Hanlon <jamie@debian-vps>
2018-06-20 05:13:02 -0400

    Tweaks to set-survey-phase.py script

--- scripts/set-survey-phase.py
+++ scripts/set-survey-phase.py
@@ -8,16 +8,18 @@ config=dict()
 app = create_app(config)
 
 with app.app_context():
-    for phase, start_date in enumerate(PHASE_START_DATES):
+    for i, start_date in enumerate(PHASE_START_DATES):
+        phase = i + 1
         end_date = start_date + datetime.timedelta(365.25)
         query = Surveys.query;
         query = query.filter(Surveys.survey_request_date >= start_date)
         query = query.filter(Surveys.survey_request_date < end_date)
         for survey in query.all():
-            survey.phase = phase
-            print 'Set phase {} for survey {}'.format(phase, survey.id)
+            if survey.phase == -1 or survey.phase == None:
+              survey.phase = phase
+              print 'Set phase {} for survey {}'.format(phase, survey.id)
     db.session.commit()
     print 'Surveys without a phase:'
-    query = Surveys.query.filter(phase==-1)
+    query = Surveys.query.filter(phase==None or phase==-1)
     for survey in query.all():
         print '  id {} in phase {}'.format(survey.id, survey.phase)
