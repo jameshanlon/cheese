cf1011efab47f8237eea05ab995c74c6f83fc3fa
James Hanlon <jamie@debian-vps>
2018-10-17 06:01:32 -0400

    Tweaks for DB enum updates

--- scripts/update-enums-surveys.py
+++ scripts/update-enums-surveys.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 from cheese.factory import create_app
-from cheese.models import db, Surveys, BuildingTypes, LeadStatusTypes 
+from cheese.models import db, Surveys, BuildingTypes, SurveyLeadStatuses, Wards
 
 app = create_app()
 f = open(sys.argv[1], 'r')
@@ -10,7 +10,8 @@ for row in reader:
     print 'Result {}'.format(row)
     with app.app_context():
       result = Surveys.query.get(int(row[0]))
-      result.building_type_id = BuildingTypes.query.filter_by(name=row[1].strip()).first().id
-      result.lead_status_id = SurveyLeadStatuses.query.filter_by(name=row[2].strip()).first().id
+      result.ward_id = Wards.query.filter_by(name=row[1].strip()).first().id
+      #result.lead_status_id = SurveyLeadStatuses.query.filter_by(name=row[1].strip()).first().id
+      #result.building_type_id = BuildingTypes.query.filter_by(name=row[2].strip()).first().id
       db.session.commit()
 f.close()
