e252b7ffc5a90907a27e9c3c08b0cdd67513bca6
James Hanlon <mail@jameswhanlon.com>
2023-08-08 15:28:18 -0400

    Export db instructions

--- scripts/export-db.sh
+++ scripts/export-db.sh
@@ -1,20 +1,35 @@
+# Create ~/.my.cnf with:
+#
+#   [client]
+#   user=root
+#   password=...
+#
+# before running this to avoid password prompts.
 for x in \
-	member \
-	month_feedback \
-	post_survey_details \
-	pre_survey_details \
-	results \
-	space_heating_types \
-	survey_lead_statuses \
-	surveys \
-	thermal_image \
-	wards \
-	year_feedback \
-	water_heating_types \
-	wall_construction_types \
-	building_types cooking_types; do \
+alembic_version \
+building_types \
+cooking_types \
+inventory \
+kits \
+member \
+month_feedback \
+occupation_types \
+post_survey_details \
+pre_survey_details \
+results \
+role \
+space_heating_types \
+survey_lead_statuses \
+surveys \
+thermal_image \
+user \
+user_invite \
+user_roles \
+wall_construction_types \
+wards \
+water_heating_types \
+year_feedback; \
+do
   echo "Exporting $x"
-  COMMAND="select * into outfile '/tmp/mysql-export-$x.csv' fields terminated by ',' optionally enclosed by '\"' lines terminated by '\n' from cheese.$x;"
-  COMMAND="desc cheese.$x"
   mysql -e "select * into outfile \"/tmp/mysql-export-$x.csv\" fields terminated by ',' optionally enclosed by '\"' lines terminated by '\n' from cheese.$x"
 done
